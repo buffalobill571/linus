9711cc415e0756a4654234d66fcb9017c2b55682
buffalobill571 <bdabylovb@gmail.com>
2018-08-06 18:15:18 +0600

    good

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -5,15 +5,28 @@ from apps.blog.models import Post, FAQ, Tag
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
     search_fields = ('name',)
 
 
 @admin.register(FAQ)
 class FAQAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('pk', 'truncated_question', 'truncated_answer')
+    search_fields = ('answer', 'question')
+
+    def truncated_question(self, obj):
+        return obj.question[:50]
+
+    def truncated_answer(self, obj):
+        return obj.answer[:50]
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'published_date', 'truncated_body')
     search_fields = ('title', 'body')
     autocomplete_fields = ('tags',)
+    list_filter = ('tags',)
+
+    def truncated_body(self, obj):
+        return obj.body[:50]
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -12,7 +15,13 @@ class Post(models.Model):
     image = models.FileField(upload_to='blog/posts/')
     published_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class FAQ(models.Model):
     question = models.TextField()
     answer = models.TextField()
+
+    def __str__(self):
+        return self.question[:30]
