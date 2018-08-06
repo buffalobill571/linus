962a38b22677c6fd23f3eeb4432521ed44b8e213
buffalobill571 <bdabylovb@gmail.com>
2018-08-06 16:25:58 +0600

    azaza

--- config/settings.py
+++ config/settings.py
@@ -146,5 +146,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_ROOT = str(public_root.path('staticfiles'))
+STATIC_URL = '/staticfiles/'
