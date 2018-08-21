ca3628cccd41029f5a5608de91c106a1e2947cc0
buffalobill571 <bdabylovb@gmail.com>
2018-08-21 23:55:59 +0600

    azaza

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20
 }
